db89bff811af1489f31677c4547328e5e9ec026d
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-21 19:57:25 -0800

    Creates outcome controller, get outcome GET method

---
+++ GameManager/Controllers/OutcomeController.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using GameManager.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OutcomeController : ControllerBase
+    {
+        private readonly IOutcomeService outcomeService;
+
+        public OutcomeController()
+        {
+            outcomeService = new OutcomeService();
+        }
+
+        [HttpGet("{userMove}")]
+        public IActionResult GetOutcome(int userMove)
+        {
+            return Ok(outcomeService.GetOutcome(userMove));
+        }
+    }
+}
